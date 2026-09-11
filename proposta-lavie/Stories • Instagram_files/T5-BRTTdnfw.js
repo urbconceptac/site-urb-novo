3fd2909db651c63192def595ba428a3a7e3f8f41
urbconceptac <urbconceptac@gmail.com>
2026-09-11 14:15:27 -0300

    Adiciona pasta proposta-lavie para apresentação ao cliente

---
+++ proposta-lavie/Stories • Instagram_files/T5-BRTTdnfw.js
@@ -0,0 +1,3 @@
+;/*FB_PKG_DELIM*/
+
+__d("isPolarisImmersiveViewerEnabled",["gkx"],(function(t,n,r,o,a,i,l){"use strict";function e(){return r("gkx")("26826")}l.default=e}),98);
---
+++ proposta-lavie/Stories • Instagram_files/xHp6qaH7ddV.js
@@ -0,0 +1,3 @@
+;/*FB_PKG_DELIM*/
+
+__d("focusKeyboardEventPropagation",[],(function(t,n,r,o,a,i){"use strict";function e(e){return e._stopFocusKeyboardPropagation===!0}function l(e){e._stopFocusKeyboardPropagation=!0}i.hasFocusKeyboardEventPropagationStopped=e,i.stopFocusKeyboardEventPropagation=l}),66);
